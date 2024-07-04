e467e7d66e9f24cda07e6c0a29e9d086f33b074e
Laydine <laydineakatch@gmail.com>
2024-07-04 11:09:01 +0300

    Added an example activity

---
+++ app/src/main/java/com/example/reviewapp/Example.kt
@@ -0,0 +1,13 @@
+package com.example.reviewapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Example : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_example)
+
+        var num: Int = 10
+    }
+}
